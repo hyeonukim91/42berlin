456ea973d0b0d3ff7b53c573f71317fa2a1c7cdb
hyeonukim91 <hyeonukim91@gmail.com>
2024-12-01 19:29:47 +0100

    update working progress at the cluster

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,8 +6,14 @@
 /*   By: hyeonuki <hyeonuki@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 18:03:32 by hyeonuki          #+#    #+#             */
-/*   Updated: 2024/12/01 18:23:47 by hyeonuki         ###   ########.fr       */
+/*   Updated: 2024/12/01 19:23:04 by hyeonuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *str);
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+
+	va_start (args, format);
+
+}
---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeonuki <hyeonuki@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 19:13:19 by hyeonuki          #+#    #+#             */
+/*   Updated: 2024/12/01 19:15:35 by hyeonuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
+
