11294ac69927d4917b640331cda9b6db97994761
Hyeonu Kim <hyeonukim@Hyeonus-MacBook-Air.local>
2025-05-13 18:54:16 +0200

    process plan

---
+++ rank02/pipex/pipex.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeonukim <hyeonukim@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 18:38:23 by hyeonukim         #+#    #+#             */
+/*   Updated: 2025/05/13 18:53:25 by hyeonukim        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+
+process flow
+
+1. check arguments
+
+2. read from infile
+
+3. make it stdin and run command 1
+
+4. output of 3 should be input of command 2
+
+5. run command 2 and save the stdout to outfile
+
+
+required functions
+1. arguments checking -> can be done in the main function
+
+2. memory cleaning -> when a functions fails
+
+3. actual piping function
+
+4. parsing functions
+
+*/
