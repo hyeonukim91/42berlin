b5f71201ad1c7ca90608b614e53534186c956914
hyeonukim91 <hyeonukim91@gmail.com>
2025-05-25 18:54:01 +0200

    pipe xmain

--- rank02/pipex/pipex.c
+++ rank02/pipex/pipex.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeonukim <hyeonukim@student.42.fr>        +#+  +:+       +#+        */
+/*   By: hyeonuki <hyeonuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 18:38:23 by hyeonukim         #+#    #+#             */
-/*   Updated: 2025/05/16 15:18:10 by hyeonukim        ###   ########.fr       */
+/*   Updated: 2025/05/25 18:53:33 by hyeonuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# include "pipex.h"
 
 /*
 
@@ -33,14 +35,37 @@ required functions
 2. memory cleaning -> when a functions fails
 
 3. actual piping function
-
+.
 4. parsing functions
 
 */
 
-int	main(int argc, char **argv)
+
+int	main(int argc, char **argv, )
 {
+	/*check the access of the input file*/
+	int		fds[2];
+	pid_t	pid;
+
+	if (argc != 5)
+	{
+		//exit
+	}
+	if (pipe(fds) == -1)
+	{
+		//exit 
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		//exit
+	}
 	
+		
 
 }
 
+int child_read(int fd, char *path)
+{
+	
+}
--- rank02/pipex/pipex.h
+++ rank02/pipex/pipex.h
@@ -3,10 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeonukim <hyeonukim@student.42.fr>        +#+  +:+       +#+        */
+/*   By: hyeonuki <hyeonuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 18:38:31 by hyeonukim         #+#    #+#             */
-/*   Updated: 2025/05/06 18:38:32 by hyeonukim        ###   ########.fr       */
+/*   Updated: 2025/05/25 15:30:51 by hyeonuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+
+#endif
